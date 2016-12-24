e310d6d45c228b358ef454a3ec23b015335a667b
JasonQSY <syqian@sjtu.edu.cn>
2016-12-24 23:56:42 +0800

    move static tools to this repo

---
+++ content/pages/about.md
@@ -0,0 +1,30 @@
+Title: About me
+Date: 2016-2-10
+Modified: 2016-11-19
+
+## Profile
+
+My name is Qian Shengyi (JasonQSY), a sophomore student in Univerisity of Michigan - Shanghai Jiao Tong Univerisity Joint Intitute (UM-SJTU JI). My major is Electronic and Computer Engineering (ECE). There are several courses I take
+
+- VG101 Introduction to Engineering.
+- VE203 Discrete Mathematics.
+- VE280 Programming and Data Structure.
+
+## Skills
+
+- Web development: `bootstrap`, `angularjs`, `PHP`, `Laravel`, `Wordpress`, Chrome extension...
+- Data analysis: `jupyter notebook`, `numpy`, `pandas`, `matplotlib`...
+- Algorithms and data structure: `C++`
+- Batch: `python`
+
+## Experience
+
+- Member of [dyWeb](https://github.com/dyWeb/).
+- Developer and one of maintainers of [tongqu website](http://tongqu.me), which is the biggest website in SJTU for students' activities.
+- Developer of [SJTU military training website](http://junxun.sjtu.edu.cn).
+- Build my own techical [blog](http://blog.jasonqian.me) based on Pelican.
+
+## Contact
+
+- Email: jasonsyqian@gmail.com
+- Github: <https://github.com/JasonQSY>
---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Jason Qian'
+SITENAME = 'JasonQian'
+SITEURL = 'localhost:8000'
+TIMEZONE = 'Asia/Shanghai'
+
+PATH = 'content'
+
+DEFAULT_LANG = 'en'
+
+THEME = "themes/pelican-elegant"
+
+# Plugins
+PLUGIN_PATHS = ["plugins"]
+
+PLUGINS = ["render_math"]
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('email', 'mailto:syqian@sjtu.edu.cn'),
+          ('github', 'http://github.com/JasonQSY'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+GOOGLE_ANALYTICS = 'UA-73612193-1'
+DISQUS_SITENAME = "jasonqian"
+GITHUB_URL = 'http://github.com/JasonQSY'
